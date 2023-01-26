812f41b6731d815d481dd19799bd82c9650f8405
Ngwere <ngwerecliford@gmail.com>
2023-01-26 04:22:43 +0000

    created task 1 - 15

--- printf.c
+++ printf.c
@@ -1,44 +1,76 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
-
+#include <unistd.h>
+#include "main.h"
 /**
- * _printf - Prints all of the arguments when specified
- * @format: specifies the necessary operations
- * Return: void
- */
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*find_function(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
 
+	while (find_f[i].sc)
+	{
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
+		i++;
+	}
+	return (NULL);
+}
+/**
+  * _printf - function that produces output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output text;
+  */
 int _printf(const char *format, ...)
 {
-	int counter;
-	int i;
-	char *str;
-	va_list a_list;
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-	counter = 0;
-	va_start(a_list, format);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
 	{
-		switch (format[i])
+		while (format[i] != '%' && format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(a_list, int));
-				break;
-			case 's':
-				str = va_arg(a_list, char*);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
-			default:
-				break;
+			_putchar(format[i]);
+			cprint++;
+			i++;
 		}
-		if (format[i + 1] != '\0')
-			printf(", ");
-		i++;
-		counter = i;
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	printf("\n");
-	va_end(a_list);
-	return counter;
+	va_end(ap);
+	return (cprint);
 }
